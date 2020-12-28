1797a1e86dcf3e7916a43efb85cae58bc4caaae3
Peter Taraba <taraba.peter@mail.com>
2020-12-27 19:09:59 -0500

    adding into and from monitor

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Anaglyph glasses required to enjoy this 3D image :) - or run 'python3 play.py'. The code could not get any simpler.
 
 ![Whoa](./pics/Whoa.png)
+![Whoa](./pics/Whoa02.png)
 
 You need 3D anaglyph glasses. Options:
 * [![glasses1](./pics/glasses01.png)](https://www.amazon.com/gp/product/B07NQVZM72/)
--- play.py
+++ play.py
@@ -5,10 +5,12 @@ colors = ['r','cyan']
 
 plt.figure(num='Whoa', figsize = (15,15))
 
+toFront = True
+
 for i in range(8):
-    circle1=plt.Circle((0.1 + i * 0.1, 0.1 + i * 0.1),.1,color='r',alpha=0.1)
+    circle1=plt.Circle((0.1 + i * 0.1, 0.1 + i * 0.1),.1,color='cyan' if toFront else 'r',alpha=0.1)
     plt.gcf().gca().add_artist(circle1)
-    circle1=plt.Circle((0.1 + i * 0.1 + 0.01 + 0.01 * i, 0.1 + i * 0.1),.1,color='cyan',alpha=0.1)
+    circle1=plt.Circle((0.1 + i * 0.1 + 0.01 + 0.01 * i, 0.1 + i * 0.1),.1,color='r' if toFront else 'cyan',alpha=0.1)
     plt.gcf().gca().add_artist(circle1)
 
 plt.axis('off')
