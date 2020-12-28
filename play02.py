371b6abb66faf760413ddfb61781f6b4aa679616
Peter Taraba <taraba.peter@mail.com>
2020-12-27 23:14:46 -0500

    and this even better

--- play02.py
+++ play02.py
@@ -14,9 +14,9 @@ while True:
     
     for i in range(1,9,2):
         z = 0.07 + 0.07 * math.cos(time * i * 0.1 + i)
-        circle1=plt.Circle((0.1 + i * 0.1, 0.1 + i * 0.1),.03,color='cyan' if z >= 0 else 'r',alpha=0.1)
+        circle1=plt.Circle((0.1 + i * 0.1 - z * 0.5, 0.1 + i * 0.1),.03,color='cyan' if z >= 0 else 'r',alpha=0.1)
         plt.gcf().gca().add_artist(circle1)
-        circle1=plt.Circle((0.1 + i * 0.1 + z, 0.1 + i * 0.1),.03,color='r' if z >= 0 else 'cyan',alpha=0.1)
+        circle1=plt.Circle((0.1 + i * 0.1 + z * 0.5, 0.1 + i * 0.1),.03,color='r' if z >= 0 else 'cyan',alpha=0.1)
         plt.gcf().gca().add_artist(circle1)
 
     plt.axis('off')
